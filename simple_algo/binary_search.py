a0bca1b5427f9acdd5d268ccc851f0dcbb8c3a31
Abdur Rahman tumzied <50454992+tz01x@users.noreply.github.com>
2021-07-01 08:42:33 +0600

    Update binary_search.py

--- simple_algo/binary_search.py
+++ simple_algo/binary_search.py
@@ -1,19 +1,19 @@
 def binarySearch(listitem,terget):
-  '''take a sorted listitem and a terget as an argument. return idx number of the list item  '''
-    lo=0
-    hi=len(listitem)
-    
-    while True:
-        mid=(lo+hi)//2
-        print("low: ",lo," high: ",hi," mid: ",mid)
-        
-        if(listitem[mid]==terget):
-            return mid
-        if(mid==0 or mid==len(listitem)-1):
-            break
-        if(listitem[mid]<terget):
-            lo=mid
-        if(listitem[mid]>terget):
-            hi=mid
+  '''take a sorted listitem and a terget as an argument. return idx number of the list item  '''  
+  lo=0
+  hi=len(listitem)
+  
+  while True:
+      mid=(lo+hi)//2
+      print("low: ",lo," high: ",hi," mid: ",mid)
+      
+      if(listitem[mid]==terget):
+          return mid
+      if(mid==0 or mid==len(listitem)-1):
+          break
+      if(listitem[mid]<terget):
+          lo=mid
+      if(listitem[mid]>terget):
+          hi=mid
        
-    return None
+  return None
